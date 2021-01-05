09e6781525c9d74403c89d3684585efa989514ad
earlgray <earlgray283@gmail.com>
2021-01-05 14:50:43 +0900

    fix

--- MapGameController.java
+++ MapGameController.java
@@ -19,10 +19,10 @@ public class MapGameController implements Initializable {
         mapData = new MapData(21, 15);
         chara = new MoveChara(1, 1, mapData);
         mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
-        for(int y=0; y<mapData.getHeight(); y++){
-            for(int x=0; x<mapData.getWidth(); x++){
-                int index = y*mapData.getWidth() + x;
-                mapImageViews[index] = mapData.getImageView(x,y);
+        for(int y=0; y<mapData.getHeight(); y++) {
+            for (int x = 0; x < mapData.getWidth(); x++) {
+                int index = y * mapData.getWidth() + x;
+                mapImageViews[index] = mapData.getImageView(x, y);
             }
         }
         drawMap(chara, mapData);
